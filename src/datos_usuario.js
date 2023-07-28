c1008d9b3f66fc48dc641461af360597cc964630
imaciasd <imaciasd@ipn.mx>
2023-07-28 00:49:40 -0600

    :3 iber seal of aproval

--- src/datos_usuario.js
+++ src/datos_usuario.js
@@ -1,72 +1,100 @@
 // Objeto con los datos del usuario
-const usuario = {
+/*const usuario = {
   Usuario: "usuarioEjemplo",
   Telefono: 123456789,
   Email: "usuario@example.com",
   Password: "contrasena123",
-};
+};*/
 
-  // Función para validar los datos del formulario
-  function validarDatos() {
-    const emailInput = document.getElementById('Email');
-    const telInput = document.getElementById('Telefono');
-    const usuarioInput = document.getElementById('Usuario');
-    const contasenaInput = document.getElementById('Password');
-
-    const emailREGEX = new RegExp(/^([\w\.\_])*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{3,})$/);
-    const nombreREGEX = new RegExp(/^[a-zA-z\s]{2,}$/);
-    const usuarioREGEX = new RegExp(/^[a-zA-z]{4,}$/);
-    const telREGEX = new RegExp(/^\d{10}$/);
-
-    if (!emailREGEX.test(emailInput.value) || !telREGEX.test(telInput.value) || !usuarioREGEX.test(usuarioInput.value) || !(contasenaInput.value.length >= 6)) {
-      window.alert("Formato de entrada incorrecto, revise los campos proporcionados");
-      return false; // Detener el envío del formulario si los campos no son válidos
+// Función para obtener los datos del usuario desde la API mediante un método GET
+async function obtenerDatosUsuario() {
+  try {
+    const response = await fetch("https://ojosdgato-api.up.railway.app/ojosdgato/users/1");
+    if (!response.ok) {
+      throw new Error("Error al obtener los datos del usuario.");
     }
-    return true;
+    const data = await response.json();
+    mostrarDatos(data);
+  } catch (error) {
+    console.error(error);
+    window.alert("Error al obtener los datos del usuario.");
   }
+}
 
-  // Función para mostrar los datos del objeto en los campos del formulario
-  function mostrarDatos() {
-    document.getElementById("Usuario").value = usuario.Usuario;
-    document.getElementById("Telefono").value = usuario.Telefono;
-    document.getElementById("Email").value = usuario.Email;
-    document.getElementById("Password").value = usuario.Password;
-    bloquearCampos();
-  }
+// Función para enviar los datos modificados del formulario a la API mediante un método PUT
+async function enviarDatosModificados() {
+  const usuarioModificado = {
+    name: document.getElementById("Usuario").value,
+    phone: document.getElementById("Telefono").value,
+    email: document.getElementById("Email").value,
+    password: document.getElementById("Password").value,
+  };
 
-  // Función para bloquear los campos del formulario
-  function bloquearCampos() {
-    const inputs = document.querySelectorAll("input");
-    inputs.forEach((input) => {
-      input.disabled = true;
+  try {
+    const response = await fetch("https://ojosdgato-api.up.railway.app/ojosdgato/users/14", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(usuarioModificado),
     });
+
+    if (!response.ok) {
+      throw new Error("Error al guardar los datos modificados.");
+    }
+
+    window.alert("Los datos se han guardado correctamente.");
+    bloquearCampos(); // Bloquear los campos nuevamente después de guardar
+  } catch (error) {
+    console.error(error);
+    window.alert("Error al guardar los datos modificados.");
   }
+}
 
-  // Función para habilitar los campos del formulario
-  function habilitarCampos() {
-    const inputs = document.querySelectorAll("input");
-    inputs.forEach((input) => {
-      input.disabled = false;
-    });
-  }
+// Modificar la función mostrarDatos para que reciba los datos del usuario como argumento
+function mostrarDatos(data) {
+  document.getElementById("Usuario").value = data.name;
+  document.getElementById("Telefono").value = data.phone;
+  document.getElementById("Email").value = data.email;
+  document.getElementById("Password").value = data.password;
+  bloquearCampos();
+}
 
-  // Llamar a la función mostrarDatos para llenar los campos al cargar la página
-  window.onload = mostrarDatos;
-
-  // Agregar event listener al botón "Habilitar edición" para habilitar los campos
-  document.getElementById("editar-btn").addEventListener("click", () => {
-    Swal.fire({
-      title: '¿Quieres editar tus datos?',
-      showDenyButton: true,
-      showCancelButton: false,
-      confirmButtonText: 'Si',
-      denyButtonText: `No`,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        habilitarCampos();
-      }
-    });
+// Función para bloquear los campos del formulario
+function bloquearCampos() {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    input.disabled = true;
   });
+}
+
+// Función para habilitar los campos del formulario
+function habilitarCampos() {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    input.disabled = false;
+  });
+}
+
+// Llamar a la función obtenerDatosUsuario para obtener los datos del usuario al cargar la página
+window.onload = obtenerDatosUsuario;
+
+// Agregar event listener al botón "Guardar" para enviar los datos modificados
+document.getElementById("guardar-btn").addEventListener("click", (event) => {
+  event.preventDefault(); // Evitar el envío del formulario por defecto
+  Swal.fire({
+    title: '¿Deseas guardar los cambios?',
+    showDenyButton: true,
+    showCancelButton: false,
+    confirmButtonText: 'Guardar',
+    denyButtonText: 'Cancelar',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      enviarDatosModificados();
+    }
+  });
+});
+
 
   // Agregar event listener al formulario para realizar la validación antes de guardar los cambios
   document.getElementById("formulario").addEventListener("submit", (event) => {
